3c3e4d21fd58fee6058924cf16ba3b9c84dc7e09
Rektoooooo <sebastian.kucera@icloud.com>
2026-01-17 22:20:42 +0100

    Initial Commit

---
+++ MealPrepAI/MealPrepAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPrepAIApp.swift
+//  MealPrepAI
+//
+//  Created by Sebastián Kučera on 17.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MealPrepAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MealPrepAITests/MealPrepAITests.swift
@@ -0,0 +1,17 @@
+//
+//  MealPrepAITests.swift
+//  MealPrepAITests
+//
+//  Created by Sebastián Kučera on 17.01.2026.
+//
+
+import Testing
+@testable import MealPrepAI
+
+struct MealPrepAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
